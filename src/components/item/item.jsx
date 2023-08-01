f48c2ddb1db8dbc1f3b0a5f6dea48d0c144455b2
OlekTarabesh <oleksandr.tarabesh@gmail.com>
2023-08-01 18:55:56 +0300

    Created Item component for Sidebar and styled it.

---
+++ src/components/item/item.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+import { Typography, useTheme } from '@mui/material';
+import { Menu, MenuItem } from 'react-pro-sidebar';
+import { tokens } from '../../theme';
+
+const Item = ({ title, to, icon, selected, setSelected }) => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    return (
+      <Menu>
+        <MenuItem
+          component={<Link to={to}></Link>}
+          active={selected === title}
+          style={{
+            color: colors.grey[100],
+          }}
+          onClick={() => setSelected(title)}
+          icon={icon}
+          /* Here👇🏻 is a new styling of MUI library. Check it out! */
+          rootStyles={{
+            a: {
+              [`&.ps-menu-button:hover`]: {
+                color: "#868dfb !important",
+                backgroundColor: 'transparent !important'
+              },
+              [`&.ps-menu-button.ps-active:hover`]: {
+                color: "#6870fa !important",
+                backgroundColor: "transparent !important"
+              },
+            }
+          }}
+        >
+          <Typography>{title}</Typography>
+        </MenuItem>
+      </Menu>
+    );
+  };
+
+  export default Item;
