d9d21404a60df3b3c3875276734efe546c462a21
OlekTarabesh <oleksandr.tarabesh@gmail.com>
2023-08-01 18:50:28 +0300

    Added user image, name and styled everything.

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -70,6 +70,39 @@ const Sidebarr = () => {
               </Box>
             )}
             </MenuItem>
+            
+          {/* USER */}
+          {!isCollapsed && (
+          <Box mb='25px'>
+            <Box display='flex' justifyContent='center' alignItems='center'>
+              <img 
+              src={'../../assets/user.jpeg'} 
+              alt="profile-user" 
+              width='100px'
+              height='100px'
+              style={{
+                cursor: 'pointer',
+                borderRadius: '50%',
+                objectFit: 'cover'
+              }}
+              />
+            </Box>
+            
+            <Box textAlign='center'>
+                <Typography 
+                variant='h3' 
+                color={colors.grey[100]}
+                fontWeight='bold'
+                sx={{
+                  m: '10px 0 0 0'
+                }}
+                >
+                  Oleksandr Tarabesh
+                </Typography>
+                <Typography variant='h5' color={colors.greenAccent[500]}>Admin</Typography>
+              </Box>
+            </Box>
+          )}
         </Menu>
       </Sidebar>
   )
