a3154ceeb0cd7ccde64b3bf69a62c0fc8718cbb9
OlekTarabesh <oleksandr.tarabesh@gmail.com>
2023-08-02 23:30:46 +0300

    Created Contacts route with styling. Most of this copied and pasted from Team but it has some changes.

--- src/App.js
+++ src/App.js
@@ -7,9 +7,8 @@ import Topbar from "./scenes/global/Topbar";
 import Sidebarr from "./scenes/global/Sidebar";
 import Dashboard from './scenes/dashboard/dashboard'
 import Team from "./scenes/team/team";
-// import Team from './scenes/team'
+import Contacts from "./scenes/contacts/contacts";
 // import Invoices from './scenes/invoices'
-// import Contacts from './scenes/contacts'
 // import Bar from './scenes/bar'
 // import Form from './scenes/form'
 // import Line from './scenes/line'
@@ -33,7 +32,7 @@ function App() {
               <Routes>
                 <Route path="/" element={<Dashboard />} /> 
                 <Route path="/team" element={<Team />} /> 
-                {/* <Route path="/contacts" element={<Contacts />} />  */}
+                <Route path="/contacts" element={<Contacts />} /> 
                 {/* <Route path="/invoices" element={<Invoices />} />  */}
                 {/* <Route path="/form" element={<Form />} />  */}
                 {/* <Route path="/bar" element={<Bar />} />  */}
