ec8ca720e267a9c9a372148612ba6514f1150e1f
OlekTarabesh <oleksandr.tarabesh@gmail.com>
2023-08-04 15:11:12 +0300

    Added userSchema with required fields

--- src/scenes/form/form.jsx
+++ src/scenes/form/form.jsx
@@ -16,8 +16,19 @@ const initialValues = {
 const phoneRegExp = 
     /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 
+const userSchema = yup.object().shape({
+    firstName: yup.string().required('required'),
+    lastName: yup.string().email('invalid email').required('required'),
+    email: yup.string().required('required'),
+    contact: yup
+        .string()
+        .matches(phoneRegExp, 'Phone number is not valid')
+        .required('required'),
+    address1: yup.string().required('required'),
+    address2: yup.string().required('required'),
+});
 const Form = () => {
-    
+
     return (
         <Box></Box>
     )
