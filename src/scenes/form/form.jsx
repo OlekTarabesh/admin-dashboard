e33e73e4c84effafea1f7911e8c717f11b444abe
OlekTarabesh <oleksandr.tarabesh@gmail.com>
2023-08-04 15:14:09 +0300

    Created fields of form with styling

--- src/scenes/form/form.jsx
+++ src/scenes/form/form.jsx
@@ -28,9 +28,108 @@ const userSchema = yup.object().shape({
     address2: yup.string().required('required'),
 });
 const Form = () => {
+    const isNonMobile = useMediaQuery('(min-width: 600px)');
 
+    const handleFormSubmit = (values) => {
+        console.log(values);
+    }
     return (
-        <Box></Box>
+        <Box m='20px'>
+            <Header title='CREATE USER' subtitle='Create a New User Profile' />
+
+            <Formik 
+                onSubmit={handleFormSubmit}
+                initialValues={initialValues}
+                validationSchema={userSchema}
+                >
+                {({
+                    values, 
+                    errors, 
+                    touched, 
+                    handleBlur, 
+                    handleChange, 
+                    handleSubmit
+                }) => (
+                    <form onSubmit={handleSubmit}>
+                        <Box 
+                            mt='20px'
+                            display='grid'
+                            gap='30px' 
+                            gridTemplateColumns='repeat(4, minmax(0, 1fr))'
+                            sx={{
+                                '& > div': { gridColumn: isNonMobile ? undefined : 'span 4'},
+                            }}
+                        >
+                            <TextField 
+                                fullWidth
+                                variant="filled"
+                                type="text"
+                                label="First Name"
+                                onBlur={handleBlur}
+                                onChange={handleChange}
+                                value={values.firstName}
+                                name="firstName"
+                                error={!!touched.firstName && !!errors.firstName}
+                                helperText={touched.firstName && errors.firstName}
+                                sx={{ gridColumn: 'span 2' }}
+                            />    
+                            <TextField 
+                                fullWidth
+                                variant="filled"
+                                type="text"
+                                label="Last Name"
+                                onBlur={handleBlur}
+                                onChange={handleChange}
+                                value={values.lastName}
+                                name="lastName"
+                                error={!!touched.lastName && !!errors.lastName}
+                                helperText={touched.lastName && errors.lastName}
+                                sx={{ gridColumn: 'span 2' }}
+                            />    
+                            <TextField 
+                                fullWidth
+                                variant="filled"
+                                type="text"
+                                label="Email"
+                                onBlur={handleBlur}
+                                onChange={handleChange}
+                                value={values.email}
+                                name="email"
+                                error={!!touched.email && !!errors.email}
+                                helperText={touched.email && errors.email}
+                                sx={{ gridColumn: 'span 4' }}
+                            />    
+                            <TextField 
+                                fullWidth
+                                variant="filled"
+                                type="text"
+                                label="Address 1"
+                                onBlur={handleBlur}
+                                onChange={handleChange}
+                                value={values.address1}
+                                name="address1"
+                                error={!!touched.address1 && !!errors.address1}
+                                helperText={touched.address1 && errors.address1}
+                                sx={{ gridColumn: 'span 4' }}
+                            />   
+                            <TextField 
+                                fullWidth
+                                variant="filled"
+                                type="text"
+                                label="Address 2"
+                                onBlur={handleBlur}
+                                onChange={handleChange}
+                                value={values.address2}
+                                name="address2"
+                                error={!!touched.address2 && !!errors.address2}
+                                helperText={touched.address2 && errors.address2}
+                                sx={{ gridColumn: 'span 4' }}
+                            />    
+                        </Box>
+                    </form>
+                )}    
+            </Formik>
+        </Box>
     )
 }
 
