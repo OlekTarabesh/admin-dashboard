f03e96073a11d46b95f0af73a8a2389d80359486
OlekTarabesh <oleksandr.tarabesh@gmail.com>
2023-12-01 14:08:49 +0200

    small changes in bar and barchart for adaptive layout.

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -41,7 +41,7 @@ const BarChart = ({ isDashboard = false }) => {
       }}
       keys={["hot dog", "burger", "sandwich", "kebab", "fries", "donut"]}
       indexBy="country"
-      margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+      margin={{ top: 90, right: 90, bottom: 40, left: 50 }}
       padding={0.3}
       valueScale={{ type: "linear" }}
       indexScale={{ type: "band", round: true }}
